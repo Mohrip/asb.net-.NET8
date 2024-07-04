9bfbaff7e81a03418b6d3c890f36ed786a94bc43
Mohammed Alsaif <123654677+Mohrip@users.noreply.github.com>
2024-07-04 09:43:18 +0300

    Update startup.cs

--- startup.cs
+++ startup.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.CPU;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
